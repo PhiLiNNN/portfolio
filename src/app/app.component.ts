29e5bbc01462a9c0fc56002229af0a58ae5e0efa
PhiLiNNN <philipp_wendschuch@web.de>
2024-06-11 13:57:21 +0200

    working on aot css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { AotComponent } from './aot/aot.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AotComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
