08b1e4a946c09e4c729dc684c826622cdf964114
PhiLiNNN <philipp_wendschuch@web.de>
2024-06-14 16:08:46 +0200

    added aboutMe section plus stryling

--- src/app/aot/aot.component.ts
+++ src/app/aot/aot.component.ts
@@ -5,24 +5,29 @@ import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './aot.component.html',
-  styleUrl: './aot.component.scss',
+  styleUrls: ['./aot.component.scss'],
 })
 export class AotComponent {
-  offsetX: number = 73;
-  offsetY: number = 243;
+  maskSize: number = 200;
+  offsetX: number = 100;
+  offsetY: number = 190;
   constructor(private el: ElementRef, private renderer: Renderer2) {}
+
   onMouseEnter() {
+    const underneathElement =
+      this.el.nativeElement.querySelector('.underneath');
     this.renderer.setStyle(
-      this.el.nativeElement.querySelector('.underneath'),
+      underneathElement,
       'mask-size',
-      '200px'
+      `${this.maskSize}px`
     );
     this.renderer.setStyle(
-      this.el.nativeElement.querySelector('.underneath'),
+      underneathElement,
       'transition',
       'mask-size 0.3s ease'
     );
   }
+
   onMouseLeave() {
     const underneathElement =
       this.el.nativeElement.querySelector('.underneath');
@@ -36,15 +41,22 @@ export class AotComponent {
 
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    const maskPositionX = `${mouseX - this.offsetX}px`;
-    const maskPositionY = `${mouseY - this.offsetY}px`;
+    if (window.innerWidth > 900) {
+      const rect = this.el.nativeElement.getBoundingClientRect();
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
 
-    this.renderer.setStyle(
-      this.el.nativeElement.querySelector('.underneath'),
-      'mask-position',
-      `${maskPositionX} ${maskPositionY}`
-    );
+      if (window.innerWidth <= 1200) {
+        this.offsetY = 230;
+        this.offsetX = 50;
+      }
+      const maskPositionX = `${mouseX - this.maskSize / 2 - this.offsetX}px`;
+      const maskPositionY = `${mouseY - this.maskSize / 2 - this.offsetY}px`;
+      this.renderer.setStyle(
+        this.el.nativeElement.querySelector('.underneath'),
+        'mask-position',
+        `${maskPositionX} ${maskPositionY}`
+      );
+    }
   }
 }
