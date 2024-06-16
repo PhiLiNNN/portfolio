58bb5f64a681ed2d77d00de2287f84c207ec6cef
PhiLiNNN <philipp_wendschuch@web.de>
2024-06-17 00:40:23 +0200

    stared to styl skills component

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
+  iconArr = [
+    { src: 'angular', name: 'Angular' },
+    { src: 'typescript', name: 'TypeScript' },
+    { src: 'javascript', name: 'JavaScript' },
+    { src: 'python', name: 'Python' },
+    { src: 'html', name: 'HTML' },
+    { src: 'css', name: 'CSS' },
+    { src: 'firebase', name: 'Firebase' },
+    { src: 'git', name: 'Git' },
+    { src: 'scrum', name: 'Scrum' },
+    { src: 'api', name: 'REST API' },
+    { src: 'material', name: 'Material Design' },
+  ];
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  onMouseEnter() {
+    const toSkillsArrow = this.el.nativeElement.querySelector(
+      '.to-portfolio-arrow'
+    );
+    this.renderer.addClass(toSkillsArrow, 'hovered');
+  }
 }
