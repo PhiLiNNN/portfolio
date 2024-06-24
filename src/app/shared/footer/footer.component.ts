ff934dce91d874acb4f64d096d3f01e3aa8d2411
PhiLiNNN <philipp_wendschuch@web.de>
2024-06-24 12:46:10 +0200

    added form validation

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted)
+      console.log('sadasdsadsadsad :>> ', this.contactData);
+  }
+}
