54111c25f368ee8ca51f46125113abea56aa2aff
PhiLiNNN <philipp_wendschuch@web.de>
2024-06-30 21:29:36 +0200

    added language menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { AotComponent } from './aot/aot.component';
 import { AboutMeComponent } from './aboutMe/aboutMe.component';
 import { SkillsComponent } from './skills/skills.component';
-import { importProvidersFrom } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { provideHttpClient } from '@angular/common/http';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 import { ReferencesComponent } from './references/references.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, './../assets/i18n', 'json');
-}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -35,32 +24,8 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  languages = ['tr', 'en', 'ru'];
-
-  constructor(public translate: TranslateService) {}
-  ngOnInit(): void {
-    const defaultLanguage = localStorage.getItem('language') || 'en';
-    this.translate.setDefaultLang(defaultLanguage);
-    this.translate.use(defaultLanguage);
-  }
-  changeLanguage(lang: string) {
-    this.translate.use(lang);
-    localStorage.setItem('language', lang);
+export class AppComponent {
+  constructor(public translate: TranslateService) {
+    this.translate.setDefaultLang('ru');
   }
 }
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      })
-    ),
-  ],
-});
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,42 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom([TranslateModule.forRoot(provideTranslation())]),
   ],
 };
